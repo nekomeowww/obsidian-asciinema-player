ee315ebf5ec3088337fb0b0552b3769f40774c7b
Ayaka Neko <neko@ayaka.moe>
2021-11-04 18:14:52 +0800

    feat: included asciinema player inside of the plugin

--- src/locales/lang/en.ts
+++ src/locales/lang/en.ts
@@ -5,5 +5,6 @@ export default {
     EnableOfflineSupport: 'Enable offline support',
     OfflineSupportOptionDesp: 'Allow plugin run in offline mode',
     InitializationFailedNotice: 'asciinema-player initialization failed. Plugin will fallback to use online assets. You could turn on %s option to retry this step',
+    DeletionFailedNotice: 'asciinema-player cleanup file failed. This might either happening when file permission changed or files already removed. You could turn off %s option to retry this step',
     AssetsDownloadFailedNotice: 'asciinema-player assets failed to download'
 }
--- src/locales/lang/zh-cn.ts
+++ src/locales/lang/zh-cn.ts
@@ -5,5 +5,6 @@ export default {
     EnableOfflineSupport: '激活离线支持',
     OfflineSupportOptionDesp: '允许在离线状态下使用插件',
     InitializationFailedNotice: '初始化 asciinema-player 失败，将使用在线资源，如果需要重试，可以前往设置并且打开 %s 选项',
+    DeletionFailedNotice: '删除失败，可能是文件已经不存在或是权限出现问题，如果需要重试，可以前往设置并且关闭 %s 选项',
     AssetsDownloadFailedNotice: 'asciinema-player 在线资源获取失败'
 }
